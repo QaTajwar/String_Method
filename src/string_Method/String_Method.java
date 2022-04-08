c31d852d86aece2b31add00f7d2de114759f6487
tajwar.hamid47@gmail.com <tajwar.hamid47@gmail.com>
2022-04-07 23:10:05 -0400

    first commit

---
+++ src/string_Method/String_Method.java
@@ -0,0 +1,26 @@
+package string_Method;
+
+public class String_Method {public static void main(String[] args) {
+	
+	String name ="Hello     ";
+	
+	//boolean result = name.equalsIgnoreCase("hello");
+	//int result = name.length();
+	//char result = name.charAt(2);
+	 //int result = name.indexOf("e");
+	//boolean result = name.isEmpty();
+	//String result = name.toUpperCase();
+	//String result = name.toLowerCase();
+	//String result = name.trim();
+	//String result = name.replaceAll("Hello", "hola");
+	//String result = name.replace("e", "m")
+	System.out.println(result);
+
+
+
+
+
+
+}
+
+}
